8557568aa494641ab4b97b37b216436b1e2a9077
astrhsptl <dctrback@bk.ru>
2022-06-12 15:01:08 -0400

    add readme

--- alarmer/settings.py
+++ alarmer/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p%u+zwg6)tdemhp6m!*ms5so&jnekk+_fdf_xsd!-%z#5ide75'
-
+SECRET_KEY = open('keys.txt', 'r').read()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
